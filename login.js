8bb78e889c33310e9aeb53879b4bc9111793ebad
fogray <forget871205@126.com>
2016-07-26 16:12:52 +0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,21 @@
+document.ready = function(){
+  $('#btnLogin').onclick = function(){
+      var url = 'https://dev.imaicloud.com/couchdb/_session';
+      var xhr = new XMLHttpRequest();
+      if (!('withCredentials' in xhr)) {
+          xhr = XDomainRequest();
+      }
+      xhr.open('POST', url, true);
+      xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+      xhr.onreadystatchanged = function(){
+        if (xhr.readyState == 4){
+          console.log(xhr.responseText)
+        }
+      };
+      var name = document.getElementByName('name'), pwd = .getElementByName('password');
+      xhr.send('name='+name+'&password='+pwd);
+      xhr.onload = function(){
+          console.log(xhr.responseText);
+      };
+  }
+}
