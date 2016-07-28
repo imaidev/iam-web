3b4a6e5a94ff450df1f017ebcb28f13b439b0678
fogray <forget871205@126.com>
2016-07-28 10:38:26 +0800

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,7 +1,7 @@
 function setCookie(c_name,value,expiredays) {  
   var exdate=new Date();  
   exdate.setDate(exdate.getDate()+expiredays);
-  document.cookie=c_name+ "=" +escape(value)+((expiredays==null) ? "" : ";expires="+exdate.toGMTString())+";path=/;domain=imaicloud.com";  
+  document.cookie=c_name+ "=" +value+((expiredays==null) ? "" : ";expires="+exdate.toGMTString())+";path=/;domain=imaicloud.com";  
 }
 
 function getCookie(c_name) {  
@@ -11,7 +11,7 @@ function getCookie(c_name) {
       c_start=c_start + c_name.length+1 ;  
       c_end=document.cookie.indexOf(";",c_start);  
       if (c_end==-1) c_end=document.cookie.length;  
-      return unescape(document.cookie.substring(c_start, c_end));  
+      return document.cookie.substring(c_start, c_end);  
     }   
   }  
   return "";  
