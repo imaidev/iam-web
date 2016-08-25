9c679812ed7c315ff21397693c13b95e1dda4657
fogray <forget871205@126.com>
2016-08-25 15:39:53 +0800

    Update build_server.sh

--- build_server.sh
+++ build_server.sh
@@ -9,3 +9,23 @@ cd iam
 wget https://dev.imaicloud.com/files/comic/IAM-war/iam.war
 
 sudo docker build -t iam .
+
+echo '>>> Get old container id'
+
+CID=$(docker ps | grep "iam" | awk '{print $1}')
+echo $CID
+
+docker build -t iam . | tee /dockerlogs/iam-build.log
+RESULT=$(cat /dockerlogs/iam-build.log | tail -n 1)
+#if [["$RESULT" != *Successfully*]];then
+#  exit -1
+#fi
+ 
+if [ "$CID" != "" ];then
+  echo '>>> Stopping old container'
+  docker stop $CID
+  docker rm $CID
+fi
+
+echo '>>> Starting new container'
+docker run -d -p 10040:8080 iam
