b77f8e0152534d208cd6649cb63a0da8b141266e
fogray <forget871205@126.com>
2016-08-29 11:32:48 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-mkdir iam
-
-cp Dockerfile_server iam/Dockerfile
-
-cd iam
-
-wget https://dev.imaicloud.com/files/comic/IAM-war/iam.war
-
-sudo jar -xvf iam.war
-
-echo 'dataSource.driverClassName=org.gjt.mm.mysql.Driver' > WEB-INF/classes/datasource.properties
-echo 'dataSource.url=jdbc:mysql://10.0.7.107:3306/iam?useUnicode=true&characterEncoding=utf-8&useOldAliasMetadataBehavior=true&allowMultiQueries=true' >> WEB-INF/classes/datasource.properties
-echo 'dataSource.username=root' >> WEB-INF/classes/datasource.properties
-echo 'dataSource.password=root' >> WEB-INF/classes/datasource.properties
-
-sudo docker build -t iam .
-
 echo '>>> Get old container id'
 
 CID=$(docker ps | grep "iam" | awk '{print $1}')
 echo $CID
+
+sudo docker build -t iam .
+
+touch /dockerlogs/iam-build.log
 
 docker build -t iam . | tee /dockerlogs/iam-build.log
 RESULT=$(cat /dockerlogs/iam-build.log | tail -n 1)
