4f6961a903e9164ff880807470a8f56cdb11a392
fogray <forget871205@126.com>
2016-08-31 17:53:10 +0800

    iam目录调整

---
+++ skins/js/iamui.js
@@ -0,0 +1,32 @@
+var CookieKeys = {
+	"expire": "imaicloud_expires",
+	"payload": "imaicloud_payload",
+	"algorithm":"imaicloud_md5",
+	"group":"imaicloud_role",
+}
+$(function(){
+	// 头像菜单
+	$('#userInfo').hover(function(){
+		$($(this).children("div[class!='sign']")).show(); 		
+	},function() {
+		$($(this).children("div[class!='sign']")).hide();		
+	});
+
+	var itoken = getCookie("itoken");
+	if(itoken != "" && itoken != null) {
+		var tokenInfo = itoken.split(".");
+		if(tokenInfo.length != 3) {
+			login();
+		} else {
+			var payload = $.base64.decode(tokenInfo[1]);
+			var userInfo = JSON.parse(payload);
+			$("#username").text(userInfo.uname);
+			setCookie(CookieKeys.expire, userInfo.exp);
+			setCookie(CookieKeys.payload, tokenInfo[1]);
+			setCookie(CookieKeys.algorithm, tokenInfo[2]);
+			setCookie(CookieKeys.group, userInfo.group);
+		}
+		
+	}
+});	
+
