103a11cd5a0d61ac019430786831fba4bcfb11b9
fogray <forget871205@126.com>
2016-11-25 08:43:57 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-echo '>>> Get old container id'
-
-CID=$(sudo docker ps | grep "iam" | awk '{print $1}')
+CID=$(docker ps | grep "iam" | awk '{print $1}')
 echo $CID
 
-sudo touch iam-build.log
+sudo docker build -t iam .
 
-sudo docker build -t iam . | tee iam-build.log
-
-RESULT=$(cat iam-build.log | tail -n 1)
+touch /dockerlogs/iam-build.log
+docker build -t iam . | tee /dockerlogs/iam-build.log
+RESULT=$(cat /dockerlogs/iam-build.log | tail -n 1)
 if [["$RESULT" != *Successfully*]];then
   exit -1
 fi
- 
-if [ "$CID" != "" ];then
-  echo '>>> Stop and remove old container'
-  sudo docker stop $CID
-  sudo docker rm $CID
-fi
 
-echo '>>> Starting new container'
-sudo docker run -d -p 10040:8080 iam
+#if [ "$CID" != "" ];then
+#  docker stop $CID
+#  docker rm $CID
+#fi
+
+#挂载jdbc.properties文件，动态配置数据源
+#docker run -d -p 10040:8080 iam -v /var/owncloud/data/kevin/files/iam/jdbc.properties:/usr/local/tomcat/webapps/iam/WEB-INF/classes/jdbc.properties -v /var/owncloud/data/kevin/files/iam/datasource.xml:/usr/local/tomcat/webapps/iam/WEB-INF/spring/datasource.xml
+
