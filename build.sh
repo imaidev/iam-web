9494f89598a31248418efda5c803c0a3d19b022f
fogray <forget871205@126.com>
2016-11-28 08:33:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 #1 创建构建日志文件
 buildlog=/var/owncloud/data/kevin/files/iam/logs/build.log
 touch $buildlog
-
+ 
 #2 获取已经在运行的iam的container ID
 CID=$(docker ps | grep "iam" | awk '{print $1}')
 echo "正在运行的iam容器ID$CID" >> $buildlog
@@ -23,7 +23,7 @@ if [ "$CID" != "" ];then
   echo '>>删除正在运行的iam容器' >> $buildlog
   docker rm $CID | tee -a $buildlog
 fi
-
+ 
 #6 运行构建好的iam image
 #挂载jdbc.properties文件，动态配置数据源
 docker run -d -p 10040:8080 -v /var/owncloud/data/kevin/files/iam/jdbc.properties:/usr/local/tomcat/webapps/iam/WEB-INF/classes/jdbc.properties -v /var/owncloud/data/kevin/files/iam/datasource.xml:/usr/local/tomcat/webapps/iam/WEB-INF/spring/datasource.xml iam | tee -a $buildlog
